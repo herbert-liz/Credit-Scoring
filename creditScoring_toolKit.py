ff12bc2090c75cf2cf22e8bd2b4222d3dd83354f
Herbert Lizama <herbert.lizama@outlook.com>
2024-08-25 17:37:35 -0600

    Actualizacion jupyter y funciones en toolkit

--- creditScoring_toolKit.py
+++ creditScoring_toolKit.py
@@ -4,6 +4,8 @@
 
 # Importing necessary libraries
 import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, _tree
 
 
 # Crea una lista con las variables categoricas y numericas
@@ -18,3 +20,14 @@ def var_numericas_categoricas(base: pd.DataFrame, drop_col: list = []):
             categoricas.append(variable)
 
     return numericas, categoricas
+
+
+# Calcula las categorias optimas para variables numerica usando arboles de decision
+def optimal_binning(base: pd.DataFrame, variable, target, max_depth: int = 3, min_samples_leaf: int = 0.05):
+    tree = DecisionTreeClassifier(
+        max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+    tree.fit(base[[variable]].values.reshape(-1, 1), base[[target]])
+    limites = tree.tree_.threshold[tree.tree_.threshold != -2]
+    limites = np.sort(limites)
+    limites = np.concatenate(([-np.inf], limites, [np.inf]))
+    return limites
