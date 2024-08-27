9bcaaf231ff9010ecef3ac25439524adabc6da72
Herbert Lizama <herbert.lizama@outlook.com>
2024-08-26 23:48:35 -0600

    Definimos funcion para calculo de IV y WOEs para variables categoricas

--- creditScoring_toolKit.py
+++ creditScoring_toolKit.py
@@ -31,3 +31,37 @@ def optimal_binning(base: pd.DataFrame, variable, target, max_depth: int = 3, mi
     limites = np.sort(limites)
     limites = np.concatenate(([-np.inf], limites, [np.inf]))
     return limites
+
+
+# Calcula los WOEs e IV para variables categoricas
+def calculate_iv_cat(base: pd.DataFrame, variable: str, target: pd.DataFrame):
+    # Agregamos target
+    base['target'] = target.iloc[:, 0]
+    grouped = base.groupby(variable, observed=False)[
+        'target'].agg(['count', 'sum'])
+    grouped['good'] = grouped['sum']
+    grouped['bad'] = grouped['count'] - grouped['sum']
+    grouped['good_dist'] = grouped['good'] / grouped['good'].sum()
+    grouped['bad_dist'] = grouped['bad'] / grouped['bad'].sum()
+    grouped['woe'] = np.log(grouped['good_dist'] / grouped['bad_dist'])
+    grouped['iv'] = (grouped['good_dist'] -
+                     grouped['bad_dist']) * grouped['woe']
+    grouped['iv'] = np.where((grouped['bad'] == 0) | (
+        grouped['good'] == 0), 0, grouped['iv'])
+    grouped['iv_total'] = grouped['iv'].sum()
+    grouped['dist'] = grouped['count'] / grouped['count'].sum()
+    grouped['good_cat'] = grouped['good'] / grouped['count']
+    grouped['bad_cat'] = grouped['bad'] / grouped['count']
+    grouped['good_total'] = grouped['good'].sum() / grouped['count'].sum()
+    iv = grouped['iv'].sum()
+
+    grouped = grouped.reset_index()
+    grouped = grouped.rename(columns={variable: 'categoria'})
+
+    grouped[['variable']] = variable
+    grouped = grouped[['variable', 'categoria', 'count', 'sum', 'good', 'bad', 'good_dist',
+                       'bad_dist', 'woe', 'iv', 'iv_total', 'dist', 'good_cat', 'bad_cat', 'good_total']]
+
+    iv_df = pd.DataFrame({'variable': [variable], 'IV': [iv]})
+
+    return iv_df, grouped
