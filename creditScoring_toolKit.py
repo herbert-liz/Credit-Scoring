18af0424d2f70acc9aabc34f6200439911c84058
Herbert Lizama <herbert.lizama@outlook.com>
2024-09-14 23:24:33 -0600

    Actualizamos codigo calculo IV y WOE categoricas

--- creditScoring_toolKit.py
+++ creditScoring_toolKit.py
@@ -24,32 +24,26 @@ def var_numericas_categoricas(base: pd.DataFrame, drop_col: list = []):
 # Calcula los WOEs e IV para variables categoricas
 
 
-def calculate_iv_cat(base: pd.DataFrame, target: pd.DataFrame, variable: str):
-    # Agregamos target
-    base['target'] = target.iloc[:, 0]
-    grouped = base.groupby(variable, observed=False)[
-        'target'].agg(['count', 'sum'])
-    grouped['good'] = grouped['sum']
-    grouped['bad'] = grouped['count'] - grouped['sum']
+def calculate_iv_cat(base: pd.DataFrame, variable: str, target: str):
+    grouped = base.groupby(variable, observed=False)[target].agg(cantidad='count', good='sum')
+    grouped['bad'] = grouped['cantidad'] - grouped['good']
     grouped['good_dist'] = grouped['good'] / grouped['good'].sum()
     grouped['bad_dist'] = grouped['bad'] / grouped['bad'].sum()
     grouped['woe'] = np.log(grouped['good_dist'] / grouped['bad_dist'])
-    grouped['iv'] = (grouped['good_dist'] -
-                     grouped['bad_dist']) * grouped['woe']
-    grouped['iv'] = np.where((grouped['bad'] == 0) | (
-        grouped['good'] == 0), 0, grouped['iv'])
+    grouped['iv'] = (grouped['good_dist'] - grouped['bad_dist']) * grouped['woe']
+    grouped['iv'] = np.where((grouped['bad'] == 0) | (grouped['good'] == 0), 0, grouped['iv'])
     grouped['iv_total'] = grouped['iv'].sum()
-    grouped['dist'] = grouped['count'] / grouped['count'].sum()
-    grouped['good_cat'] = grouped['good'] / grouped['count']
-    grouped['bad_cat'] = grouped['bad'] / grouped['count']
-    grouped['good_total'] = grouped['good'].sum() / grouped['count'].sum()
+    grouped['dist'] = grouped['cantidad'] / grouped['cantidad'].sum()
+    grouped['good_cat'] = grouped['good'] / grouped['cantidad']
+    grouped['bad_cat'] = grouped['bad'] / grouped['cantidad']
+    grouped['good_total'] = grouped['good'].sum() / grouped['cantidad'].sum()
     iv = grouped['iv'].sum()
 
     grouped = grouped.reset_index()
     grouped = grouped.rename(columns={variable: 'categoria'})
 
     grouped[['variable']] = variable
-    grouped = grouped[['variable', 'categoria', 'count', 'sum', 'good', 'bad', 'good_dist',
+    grouped = grouped[['variable', 'categoria', 'cantidad', 'good', 'bad', 'good_dist',
                        'bad_dist', 'woe', 'iv', 'iv_total', 'dist', 'good_cat', 'bad_cat', 'good_total']]
 
     iv_df = pd.DataFrame({'variable': [variable], 'IV': [iv]})
@@ -76,9 +70,9 @@ def calculate_iv_num(base: pd.DataFrame, target: pd.DataFrame, variable: str, bi
 
     base['categoria'] = pd.cut(base[variable], bins=bins, duplicates='drop')
     grouped = base.groupby('categoria', observed=False)[
-        'target'].agg(['count', 'sum'])
+        'target'].agg(['cantidad', 'sum'])
     grouped['good'] = grouped['sum']
-    grouped['bad'] = grouped['count'] - grouped['sum']
+    grouped['bad'] = grouped['cantidad'] - grouped['sum']
     grouped['good_dist'] = grouped['good'] / grouped['good'].sum()
     grouped['bad_dist'] = grouped['bad'] / grouped['bad'].sum()
     grouped['woe'] = np.log(grouped['good_dist'] / grouped['bad_dist'])
@@ -89,17 +83,17 @@ def calculate_iv_num(base: pd.DataFrame, target: pd.DataFrame, variable: str, bi
     grouped['iv'] = np.where((grouped['bad'] == 0) | (
         grouped['good'] == 0), 0, grouped['iv'])
     grouped['iv_total'] = grouped['iv'].sum()
-    grouped['dist'] = grouped['count'] / grouped['count'].sum()
-    grouped['good_cat'] = grouped['good'] / grouped['count']
-    grouped['bad_cat'] = grouped['bad'] / grouped['count']
-    grouped['good_total'] = grouped['good'].sum() / grouped['count'].sum()
+    grouped['dist'] = grouped['cantidad'] / grouped['cantidad'].sum()
+    grouped['good_cat'] = grouped['good'] / grouped['cantidad']
+    grouped['bad_cat'] = grouped['bad'] / grouped['cantidad']
+    grouped['good_total'] = grouped['good'].sum() / grouped['cantidad'].sum()
     iv = grouped['iv'].sum()
 
     grouped = grouped.reset_index()
     grouped = grouped.rename(columns={variable: 'categoria'})
 
     grouped[['variable']] = variable
-    grouped = grouped[['variable', 'categoria', 'count', 'sum', 'good', 'bad', 'good_dist',
+    grouped = grouped[['variable', 'categoria', 'cantidad', 'sum', 'good', 'bad', 'good_dist',
                        'bad_dist', 'woe', 'iv', 'iv_total', 'dist', 'good_cat', 'bad_cat', 'good_total']]
 
     iv_df = pd.DataFrame({'variable': [variable], 'IV': [iv]})
